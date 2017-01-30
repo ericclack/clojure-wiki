3f0d24d02073727d22dc8fe1ed0f28fac6b6b0e8
Eric Clack <eric@bright-interactive.com>
2017-01-30 17:26:02 +0000

    Tests for creating a wiki page

--- src/clj/couchdb_test/models/db.clj
+++ src/clj/couchdb_test/models/db.clj
@@ -3,3 +3,11 @@
 
 (defn wiki-page [id]
   (clutch/with-db "wiki" (clutch/get-document id)))
+
+(defn create-wiki-page [id content]
+  (clutch/with-db "wiki"
+    (clutch/put-document {:_id id :content content})))
+
+(defn remove-wiki-page [id]
+  (clutch/with-db "wiki"
+    (clutch/delete-document id)))
---
+++ test/clj/couchdb_test/test/db.clj
@@ -0,0 +1,27 @@
+(ns couchdb-test.test.db
+  (:require [clojure.test :refer :all]
+            [clojure.string :as s]
+            [couchdb-test.models.db :as db]))
+
+(def test-wiki-page "test-wiki-page-123")
+
+(defn doc-prep-fixture [f]
+  ;; Set up code
+  (db/remove-wiki-page test-wiki-page)
+  ;; The test
+  (f)
+  ;; Tear down code
+)
+
+(use-fixtures :each doc-prep-fixture)
+
+(deftest test-db
+  (testing "get a wiki page"
+    (let [result (db/wiki-page "welcome")]
+      (is (s/includes? (:content result) "Congratulations"))))
+
+  (testing "create a wiki page"
+    (let [page-id test-wiki-page
+          content "A very short page"
+          result (db/create-wiki-page page-id content)]
+      (is (= (:_id result) page-id)))))
