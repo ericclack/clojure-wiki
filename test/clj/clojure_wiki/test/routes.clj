a33697c48b6e0e5954428f35d37f2989198f332d
Eric Clack <eric@bright-interactive.com>
2017-02-13 15:01:05 +0000

    Fix for empty tags

--- test/clj/clojure_wiki/test/routes.clj
+++ test/clj/clojure_wiki/test/routes.clj
@@ -7,4 +7,10 @@
   (testing "test split of tag string into list"
     (let [tags "tag1, tag2, tag3 tag4"
           result (split-tags tags)]
-      (is (= '["tag1" "tag2" "tag3" "tag4"] result)))))
+      (is (= '["tag1" "tag2" "tag3" "tag4"] result))))
+
+  (testing "test empty tags - spaces"
+    (is (empty? (split-tags "   "))))
+
+  (testing "test empty string"
+    (is (empty? (split-tags "")))))
